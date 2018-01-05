1628ed8d43cadde5175a59b5ff6dbef99bf5ad37
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-01-05 14:03:54 +0700

    register extension

--- SamsonasikSampleBundle.php
+++ SamsonasikSampleBundle.php
@@ -2,8 +2,13 @@
 
 namespace Samsonasik\Bundle\SampleBundle;
 
+use Samsonasik\Bundle\SampleBundle\DependencyInjection\SampleBundleExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SamsonasikSampleBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+         return new SampleBundleExtension();
+    }
 }
